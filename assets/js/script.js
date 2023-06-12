b5589468a12958cfc64472de17207cb697fb1f18
Jay <j.thomastt1@gmail.com>
2023-06-12 16:36:46 -0600

    submit button working - no auto page refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 
 // DOM referencing
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector('#task-form');
 
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 //dynamically generating HTML
-var createTaskHandler =  function() {
+var createTaskHandler =  function(event) {
+    
+    event.preventDefault();
 
     var listItemEl = document.createElement("li");
 
@@ -18,5 +20,5 @@ var createTaskHandler =  function() {
 };
 
 
-// Call back function for createTaskHandler() function , and makes button work!
-buttonEl.addEventListener("click", createTaskHandler);
+// Call back function for createTaskHandler() function , and makes form work!
+formEl.addEventListener("submit", createTaskHandler);
