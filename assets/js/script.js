7ae25470e3de3f0ed86a7b6f2fe33eb8984d8c7c
Jay <j.thomastt1@gmail.com>
2023-06-12 16:44:21 -0600

    labelled

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,13 @@ var formEl = document.querySelector('#task-form');
 
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-
-//dynamically generating HTML
+// anomonous function
 var createTaskHandler =  function(event) {
     
+//stops submit button from refreshing page    
     event.preventDefault();
 
+//dynamically generating HTML
     var listItemEl = document.createElement("li");
 
     listItemEl.textContent = "This is a new task!";
@@ -20,5 +21,5 @@ var createTaskHandler =  function(event) {
 };
 
 
-// Call back function for createTaskHandler() function , and makes form work!
+// Call back function for createTaskHandler() function , and makes form submit button work work!
 formEl.addEventListener("submit", createTaskHandler);
