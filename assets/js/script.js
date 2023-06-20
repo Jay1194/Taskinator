215d7cce322d7f89adb9b01984d8b1c6ea4bb6da
Jay <j.thomastt1@gmail.com>
2023-06-20 14:16:45 -0600

    capture form field inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,20 +6,36 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // anomonous function
 var createTaskHandler =  function(event) {
-    
+
 //stops submit button from refreshing page    
-    event.preventDefault();
+event.preventDefault();
 
-//dynamically generating HTML
-    var listItemEl = document.createElement("li");
+//selects object element by attribute , .value -- will save text we write
+var taskNameInput = document.querySelector("input[name='task-name']").value;
 
-    listItemEl.textContent = "This is a new task!";
+// Gets value of select using attribute "drop-down task type"
+var taskTypeInput = document.querySelector("select[name='task-type']").value;
+console.log(taskTypeInput);
+   
+// create list item
+var listItemEl = document.createElement("li");
 
-    listItemEl.className = "task-item";
+listItemEl.className = "task-item";
 
-    tasksToDoEl.appendChild(listItemEl);
+// create div to hold task info and add to list item
+var taskInfoEl = document.createElement("div");
+
+// give a class name
+taskInfoEl.classname = "task-info";
+
+// add HTML content to div
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+listItemEl.appendChild(taskInfoEl)
+
+// add entire list item to list
+tasksToDoEl.appendChild(listItemEl);
 };
-
 
 // Call back function for createTaskHandler() function , and makes form submit button work work!
 formEl.addEventListener("submit", createTaskHandler);
